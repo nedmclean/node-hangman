a0a20d1dbc99a07f3563ee0c689e965132608af5
Mona Lisa <SwagwardDaniels@Edwards-MacBook-Pro-2.local>
2017-11-28 23:09:47 -0800

    update

--- game.js
+++ game.js
@@ -1,14 +1,25 @@
 var inquirer = require("inquirer");
 var word = require("./word");
 
+//will need functions within the constructor to start the game, 
+//to pick a random word from the word array, 
+//to display blank spaces,
+//to check guessed letters/store input from the user,
+//if the guessed letter is right, we need to alert and update the word,
+//if the word is guessed, return that they've won!
+//restart the game
 
+//starter constructor
 function Play(){
-//starts a game
+
 this.game = function(){
 	this.guesses = 12;
 	this.word();
 };
 //gives a word
-this.newWord
+this.newWord = function() {
+	var randWord = word[Math.floor(Math.random() * word.length)];
+
+}
 
 }
